bcbe032630dabf00a5cfa5af5238647b604553b9
dophuvu016@gmail.com <105868408+VuDo016@users.noreply.github.com>
2022-11-22 20:21:36 +0700

    Add container, footer, search bar, topbar

---
+++ src/component/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row style="text-align: center">
+    <el-col :span="12" :offset="6">
+      <el-divider></el-divider>
+      <el-tag>Powered by</el-tag>
+      <!-- <el-tag type="info" style="margin-left: 20px"
+        >Reference documentation</el-tag
+      > -->
+      <el-link style="margin-left: 20px" :underline="false" href=""
+        >Python</el-link
+      >
+      <el-divider direction="vertical"></el-divider>
+      <el-link :underline="false" href="">Django</el-link>
+      <el-divider direction="vertical"></el-divider>
+      <el-link :underline="false" href="https://cn.vuejs.org/index.html"
+        >Vue</el-link
+      >
+      <el-divider direction="vertical"></el-divider>
+      <el-link :underline="false" href="https://element.eleme.cn/#/zh-CN"
+        >Element</el-link
+      >
+      <el-divider direction="vertical"></el-divider>
+      <el-link :underline="false" href="">Github</el-link>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped></style>
---
+++ src/component/SearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row style="text-align: center">
+    <el-col :span="24">
+      <el-form :inline="true">
+        <el-form-item @click.native="$router.push('/')">
+          <img src="../assets/logo.png" width="40px" height="40px" />
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="keyword"
+            placeholder="type something"
+            size="large"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleSearch(keyword)"
+            >Search</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    handleSearch(keyword) {
+      this.$router.push({
+        path: "/search",
+        query: {
+          keyword: keyword,
+        },
+      });
+      // this.keyword = "";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
