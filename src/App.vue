28f70e092c4ae7a64993483bea23d484cfdb373c
thanhnhat108 <thanhnhatpct108@gmail.com>
2022-11-22 19:58:38 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import lang from "element-ui/lib/locale/lang/en";
+import locale from "element-ui/lib/locale";
+export default {
+  name: "App",
+};
+locale.use(lang);
+
+</script>
+
+<style></style>
